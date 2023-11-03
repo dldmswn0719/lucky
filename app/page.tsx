7bcdb2d2f2c5695ee5c170c3b47197464f1abc5f
dldmswn0719 <dldmswn071918@gmail.com>
2023-11-03 20:26:24 +0900

    운세 기능

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,83 @@
-import Image from 'next/image'
+'use client'
+import {useState} from 'react';
+import React from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [gender,setGender] = useState<string>("");
+    const [birthDate,setBirthDate] = useState<string>("");
+    const [month,setMonth] = useState<string>("1");
+    const [time,setTime] = useState<string>("");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const[resultToday,setResultToday] = useState(null);
+    const[resultTomorrow,setResultTomorrow] = useState(null);
+    const[resultMonth,setResultMonth] = useState(null);
+    const fetchData = async () =>{
+        const res = await fetch(`/api?gender=${gender}&birthdate=${birthDate}&month=${month}&time=${time}`)
+        const data = await res.json()
+        setResultToday(data.result.day);
+        setResultTomorrow(data.result.tomorrow);
+        setResultMonth(data.result.month);
+        console.log(data.result.day);
+        console.log(data.result.tomorrow);
+        console.log(data.result.month);
+        ;    }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const birthChange = ((e:React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if(value.length <=8 && /^[0-9]*$/.test(value)){
+            setBirthDate((value))
+        }
+    })
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    return (
+        <>
+            <div className="w-full px-5">
+                <div className="max-w-7xl mx-auto mt-10">
+                    <h1 className="text-3xl text-center">운세</h1>
+                    <div>
+                        <p>성별을 골라주세요</p>
+                        <button className="px-5 py-3 border w-1/2" onClick={()=>{setGender("m")}}>남자</button>
+                        <button className="px-5 py-3 border w-1/2" onClick={()=>{setGender("f")}}>여자</button>
+                    </div>
+                    <div>
+                        <p>생년월일을 입력해주세요</p>
+                        <span>생년월일</span>
+                        <input type="text" onChange={birthChange} placeholder="생년월일(8자리)" value={birthDate} />
+                    </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+                    <div>
+                        <span>달</span>
+                        <select value={month} onChange={(e)=>setMonth(e.target.value)}>
+                            <option value="1">양력</option>
+                            <option value="2">음력 평달</option>
+                            <option value="3">음력 윤달</option>
+                        </select>
+                    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                    <div>
+                        <span>시간</span>
+                        <select value={time} onChange={(e)=>setTime(e.target.value)}>
+                            <option value="">몰라요</option>
+                            <option value="0">23:30 ~ 01:29</option>
+                            <option value="1">1:30 ~ 03:29</option>
+                            <option value="2">3:30 ~ 05:29</option>
+                            <option value="3">5:30 ~ 07:29</option>
+                            <option value="4">7:30 ~ 09:29</option>
+                            <option value="5">9:30 ~ 11:29</option>
+                            <option value="6">11:30 ~ 13:29</option>
+                            <option value="7">13:30 ~ 15:29</option>
+                            <option value="8">15:30 ~ 17:29</option>
+                            <option value="9">17:30 ~ 19:29</option>
+                            <option value="10">19:30 ~ 21:29</option>
+                            <option value="11">21:30 ~ 23:29</option>
+                        </select>
+                    </div>
+                    <button className="border px-5 py-2" onClick={fetchData}>확인</button>
+                </div>
+            </div>
+            {
+                resultToday && resultToday.content[0].desc
+            }
+        </>
+    )
 }

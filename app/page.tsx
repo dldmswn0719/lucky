520bae722320f64a2c3f5c564d1d62bedfa69d1c
이은주 <dldmswn071918@gmail.com>
2023-11-17 16:16:53 +0900

    반응형 수정중

--- app/page.tsx
+++ app/page.tsx
@@ -61,27 +61,24 @@ export default function Home() {
                     <h1 className="text-4xl text-center">운세</h1>
                     <div className="pt-5 text-lg">
                         <p className="pb-5">성별을 골라주세요</p>
-                        <button className="px-8 py-3 border rounded-3xl mr-5" onClick={()=>{setGender("m")}}>남자🧑🏻</button>
-                        <button className="px-8 py-3 border rounded-3xl" onClick={()=>{setGender("f")}}>여자👧🏻</button>
+                        <button className="md:w-1/3 px-3 py-3 border rounded-3xl mr-5" onClick={()=>{setGender("m")}}>남자🧑🏻</button>
+                        <button className="md:w-1/3 px-3 py-3 border rounded-3xl" onClick={()=>{setGender("f")}}>여자👧🏻</button>
+                    </div>                   
+                    <div className="pt-5 text-lg">
+                        <p className="pb-5">🎉생년월일을 입력해주세요</p>
+                        <input className="w-full md:w-1/2 lg:w-1/3 border py-2 px-5 rounded-3xl" type="text" onChange={birthChange} placeholder="생년월일(8자리)" value={birthDate} />
                     </div>
-                    <div className="flex">
-                        <div className="pt-5 text-lg w-1/4">
-                            <p className="pb-5">🎉생년월일을 입력해주세요</p>
-                            <input className="w-full border py-2 px-5 rounded-3xl" type="text" onChange={birthChange} placeholder="생년월일(8자리)" value={birthDate} />
-                        </div>
-                        <div className="pl-10 pt-5 text-lg w-1/4">
-                            <p className="pb-5">양력,음력을 골라주세요</p>
-                            <select className="w-full border py-2 px-5 rounded-3xl" value={month} onChange={(e)=>setMonth(e.target.value)}>
-                                <option value="1">양력</option>
-                                <option value="2">음력 평달</option>
-                                <option value="3">음력 윤달</option>
-                            </select>
-                        </div>
-
+                    <div className="pt-5 text-lg">
+                        <p className="pb-5">📆양력,음력을 골라주세요</p>
+                        <select className="w-full md:w-1/2 lg:w-1/3 border py-2 px-5 rounded-3xl" value={month} onChange={(e)=>setMonth(e.target.value)}>
+                            <option value="1">양력</option>
+                            <option value="2">음력 평달</option>
+                            <option value="3">음력 윤달</option>
+                        </select>
                     </div>
                     <div className="pt-5 text-lg">
                         <p className="pb-5">⏰태어난 시간</p>
-                        <select className="w-1/4 border py-2 px-5 rounded-3xl" value={time} onChange={(e)=>setTime(e.target.value)}>
+                        <select className="w-full md:w-1/2 lg:w-1/3 border py-2 px-5 rounded-3xl" value={time} onChange={(e)=>setTime(e.target.value)}>
                             <option value="">몰라요</option>
                             <option value="0">23:30 ~ 01:29</option>
                             <option value="1">1:30 ~ 03:29</option>

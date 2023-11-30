841eaeea0d70b620cb972b32448d7472a410cc77
이은주 <dldmswn071918@gmail.com>
2023-11-30 11:53:57 +0900

    디자인 수정중

--- app/page.tsx
+++ app/page.tsx
@@ -21,34 +21,25 @@ export default function Home() {
     const [birthDate,setBirthDate] = useState<string>("");
     const [month,setMonth] = useState<string>("1");
     const [time,setTime] = useState<string>("");
-    
-    const[resultToday,setResultToday] = useState<today | null>(null);
-    const[resultTomorrow,setResultTomorrow] = useState<today | null>(null);
-    const[resultMonth,setResultMonth] = useState<today | null>(null);
-    const[dataResult,setDataResult] = useState<[today[] | null]>([null])
+    const [dataResult,setDataResult] = useState<today[] | null>(null)
 
     const fetchData = async () =>{
         if (!gender) {
-            alert("성별을 입력해주세요.");
+            alert("성별을 선택해주세요.");
             return;
         } 
         if (!birthDate) {
             alert("생년월일을 입력해주세요.");
             return;
         } 
+        
         const res = await fetch(`/api?gender=${gender}&birthdate=${birthDate}&month=${month}&time=${time}`)
         const data = await res.json()
+        data.result.day.content.pop();
+        data.result.tomorrow.content.pop(); 
         
-        setResultToday(data.result.day);
-        setResultTomorrow(data.result.tomorrow);
-        setResultMonth(data.result.month);
-        // console.log(data.result.day);
-        // console.log(data.result.tomorrow);
-        // console.log(data.result.month);
+        setDataResult([data.result.day,data.result.tomorrow, data.result.month ])
     }
-    useEffect(()=>{
-        console.log(dataResult)
-    },[dataResult])
 
     const birthChange = ((e:React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -56,10 +47,9 @@ export default function Home() {
             setBirthDate((value))
         }
     })
-    const [selectedCategory, setSelectedCategory] = useState<string>('today');
-    // const [selectedCategory, setSelectedCategory] = useState<number>(0);
-    const menuList = ['오늘', "내일", "한달"];
-    const dataList = [resultToday,resultTomorrow,  resultMonth]
+
+    const [selectedCategory, setSelectedCategory] = useState<number>(0);
+    const menuList = ['오늘의 운세', "내일의 운세", "한달 운세"];
 
     return (
         <>
@@ -106,89 +96,34 @@ export default function Home() {
                             </select>
                         </div>
                         <div className="pt-8 text-lg">
-                            <button className="border w-full px-5 py-3 rounded-md bg-cyan-600 text-white" onClick={()=>{fetchData();}}>확인</button>
+                            <button className="border w-full px-5 py-3 rounded-md bg-cyan-600 text-white" onClick={fetchData}>확인</button>
                         </div>
-                    </div>                   
-                </div>
-                <div className="max-w-7xl mx-auto px-5">
-                    <div className='flex gap-x-2 p-2'>
-                        {/* {
-                            menuList.map((e,i)=>{
-                                return(
-                                    <button key={i} className={`border py-1 px-2 ${selectedCategory === i ? 'bg-cyan-700 text-white' : ''}`} onClick={() => setSelectedCategory(i)}>{e}</button>
+                    </div>
+                    {
+                        dataResult &&
+                        <div className="flex gap-x-2 mt-5">
+                            {
+                                menuList.map((e,i)=>{
+                                    return(
+                                        <button key={i} className={`border py-1 px-2 rounded-md ${selectedCategory === i ? 'bg-cyan-600 text-white' : ''}`} onClick={() => setSelectedCategory(i)}>{e}</button>
+                                    )
+                                })
+                            }
+                        </div>
+                    }                   
+                    <div className='py-3'>
+                        {
+                            dataResult && dataResult[selectedCategory].content.map((e,i)=>{
+                                return (
+                                    <div key={i} className="pt-3">
+                                        <p className='p-2 bg-cyan-600 text-white'>{e.name}</p>
+                                        <p className='pt-3'>{e.desc}</p>
+                                    </div>
                                 )
                             })
-                        } */}
-                        
-                        {/* {
-                          resultToday && resultTomorrow && resultMonth && dataList[selectedCategory] && dataList[selectedCategory].map((e,i)=>{
-                                return(
-                                    <p key={i}>{e.desc}</p>
-                                )
-                            })
-                        } */}
-
-                        
-
-                        
-                        {/* <button className={`border py-1 px-2 ${selectedCategory === 'today' ? 'bg-cyan-700 text-white' : ''}`} onClick={() => setSelectedCategory(0)}>오늘의 운세</button>
-                        <button className={`border py-1 px-2 ${selectedCategory === 'tomorrow' ? 'bg-cyan-700 text-white' : ''}`} onClick={() => setSelectedCategory(1)}>내일의 운세</button>
-                        <button className={`border py-1 px-2 ${selectedCategory === 'month' ? 'bg-cyan-700 text-white' : ''}`} onClick={() => setSelectedCategory(2)}>한달 운세</button> */}
+                        }              
                     </div>
                 </div>
-
-             
-                {
-                    selectedCategory === 'today' && resultToday &&
-                    <div className="max-w-7xl px-5 mx-auto">
-                        <p className='text-3xl'>오늘의 운세</p>
-                        <p className='text-2xl'>{resultToday.date}</p>
-                        {
-                            resultToday.content.slice(0, -1).map((e,i)=>{
-                                return(
-                                    <div key={i}>
-                                        <h3 className="text-xl border-y bg-cyan-600 text-white py-3 pl-5">{e.name}</h3>
-                                        <p className='text-base py-3'>{e.desc}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                }
-                {
-                    selectedCategory === 'tomorrow' && resultTomorrow &&
-                    <div className="max-w-7xl px-5 mx-auto">
-                        <p className='text-3xl'>내일의 운세</p>
-                        <p className='text-2xl'>{resultTomorrow.date}</p>
-                        {
-                            resultTomorrow.content.slice(0, -1).map((e,i)=>{
-                                return(
-                                    <div key={i}>
-                                        <h3 className="text-xl border-y bg-cyan-600 text-white py-3 pl-5">{e.name}</h3>
-                                        <p className='text-base py-3'>{e.desc}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                }
-                {
-                    selectedCategory === 'month' && resultMonth &&
-                    <div className="max-w-7xl px-5 mx-auto">
-                        <p className='text-3xl'>한달의 운세</p>
-                        <p className='text-2xl'>{resultMonth.date}</p>
-                        {
-                            resultMonth.content.map((e,i)=>{
-                                return(
-                                    <div key={i}>
-                                        <h3 className="text-xl border-y bg-cyan-600 text-white py-3 pl-5">{e.name}</h3>
-                                        <p className='text-base py-3'>{e.desc}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                }
             </div>
         </>
     )

fff707e2b4e2d0f82dec20c37845b9f00f20c0ff
dldmswn0719 <dldmswn071918@gmail.com>
2023-12-05 16:37:29 +0900

    리드미 초안 작성

--- README.md
+++ README.md
@@ -1,36 +1,14 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# 운세 🔮
+운세 API 를 활용하여 기본적인 운세 정보를 제공하는 웹사이트입니다.
 
-## Getting Started
+🔗 LINK : https://lucky-phi.vercel.app/
 
-First, run the development server:
+💻 PERIOD : 2023.11.03 ~ 11.06
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+🔨 STACK
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+<img src="https://img.shields.io/badge/Typescript-3178C6?style=for-the-badge&logo=Typescript&logoColor=white">
+<img src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=Next.js&logoColor=white">
+<img src="https://img.shields.io/badge/tailwindcss-06B6D4?style=for-the-badge&logo=tailwindcss&logoColor=white">
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import {useState} from 'react';
+import { useState } from 'react';
 import React from 'react';
 
-interface today{
+interface today {
     title: string;
     date: string;
     content: contentInter[]
@@ -10,39 +10,39 @@ interface today{
 
 interface contentInter {
     name: string;
-    desc : string;
-    keyword ?: string;
-    index ?: string
+    desc: string;
+    keyword?: string;
+    index?: string
 }
 
 export default function Home() {
-    const [gender,setGender] = useState<string>("");
-    const [birthDate,setBirthDate] = useState<string>("");
-    const [month,setMonth] = useState<string>("1");
-    const [time,setTime] = useState<string>("");
-    const [dataResult,setDataResult] = useState<today[] | null>(null)
+    const [gender, setGender] = useState<string>("");
+    const [birthDate, setBirthDate] = useState<string>("");
+    const [month, setMonth] = useState<string>("1");
+    const [time, setTime] = useState<string>("");
+    const [dataResult, setDataResult] = useState<today[] | null>(null)
 
-    const fetchData = async () =>{
+    const fetchData = async () => {
         if (!gender) {
             alert("성별을 선택해주세요.");
             return;
-        } 
+        }
         if (!birthDate) {
             alert("생년월일을 입력해주세요.");
             return;
-        } 
-        
+        }
+
         const res = await fetch(`/api?gender=${gender}&birthdate=${birthDate}&month=${month}&time=${time}`)
         const data = await res.json()
         data.result.day.content.pop();
-        data.result.tomorrow.content.pop(); 
-        
-        setDataResult([data.result.day,data.result.tomorrow, data.result.month ])
+        data.result.tomorrow.content.pop();
+
+        setDataResult([data.result.day, data.result.tomorrow, data.result.month])
     }
 
-    const birthChange = ((e:React.ChangeEvent<HTMLInputElement>) => {
+    const birthChange = ((e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
-        if(value.length <=8 && /^[0-9]*$/.test(value)){
+        if (value.length <= 8 && /^[0-9]*$/.test(value)) {
             setBirthDate((value))
         }
     })
@@ -52,25 +52,25 @@ export default function Home() {
 
     return (
         <>
-            <div className="w-full px-[2%] flex justify-center items-center h-screen">
-                <div className="max-w-7xl mx-auto px-5 lg:basis-1/2 md:basis-4/5 basis-full border py-5 rounded-md">
+            <div className={`w-full px-[2%] flex justify-center ${dataResult ? 'mt-10' : 'mt-40'}`}>
+                <div className="max-w-7xl mx-auto px-5 lg:basis-[70%] md:basis-4/5 basis-[90%] border py-5 rounded-md">
                     <h1 className="text-3xl md:text-4xl text-center">운세 🔮</h1>
-                    <p onClick={()=>{location.reload();}} className='mt-10 cursor-pointer text-right'>다시하기</p>
+                    <p onClick={() => { location.reload(); }} className='mt-10 cursor-pointer text-right'>다시하기</p>
                     <div className="">
                         <div className="text-lg flex flex-wrap items-center">
                             <p className='mr-2'>성별</p>
                             <div className="flex gap-x-2">
-                                <button className={`py-1 px-3 border rounded-md ${gender === 'm' ? 'bg-blue-500 text-white' : ''}`} onClick={()=>{setGender("m")}}>남자🧑🏻</button>
-                                <button className={`py-1 px-3 border rounded-md ${gender === 'f' ? 'bg-pink-500 text-white' : ''}`}  onClick={()=>{setGender("f")}}>여자👧🏻</button>
+                                <button className={`py-1 px-3 border rounded-md ${gender === 'm' ? 'bg-blue-500 text-white' : ''}`} onClick={() => { setGender("m") }}>남자🧑🏻</button>
+                                <button className={`py-1 px-3 border rounded-md ${gender === 'f' ? 'bg-pink-500 text-white' : ''}`} onClick={() => { setGender("f") }}>여자👧🏻</button>
                             </div>
-                        </div>                   
+                        </div>
                         <div className="pt-8 text-lg flex flex-wrap items-center gap-x-2">
                             <p>🎉 생년월일</p>
                             <input className="border py-1 md:px-3 px-1 rounded-md" type="text" onChange={birthChange} placeholder="생년월일(8자리)" value={birthDate} />
                         </div>
                         <div className="pt-8 text-lg flex flex-wrap items-center gap-x-2">
                             <p>📆 양력 , 음력</p>
-                            <select className="border py-1 px-3 rounded-md" value={month} onChange={(e)=>setMonth(e.target.value)}>
+                            <select className="border py-1 px-3 rounded-md" value={month} onChange={(e) => setMonth(e.target.value)}>
                                 <option value="1">양력</option>
                                 <option value="2">음력 평달</option>
                                 <option value="3">음력 윤달</option>
@@ -78,7 +78,7 @@ export default function Home() {
                         </div>
                         <div className="pt-8 text-lg flex flex-wrap items-center gap-x-2">
                             <p>⏰ 태어난 시간</p>
-                            <select className="border py-1 px-3 rounded-md" value={time} onChange={(e)=>setTime(e.target.value)}>
+                            <select className="border py-1 px-3 rounded-md" value={time} onChange={(e) => setTime(e.target.value)}>
                                 <option value="">몰라요</option>
                                 <option value="0">23:30 ~ 01:29</option>
                                 <option value="1">1:30 ~ 03:29</option>
@@ -98,31 +98,35 @@ export default function Home() {
                             <button className="border w-full px-5 py-3 rounded-md bg-cyan-600 text-white" onClick={fetchData}>확인</button>
                         </div>
                     </div>
-                    {
-                        dataResult &&
+                </div>
+            </div>
+            <div className="w-full px-[2%] flex justify-center">
+                {
+                    dataResult &&
+                    <div className="max-w-7xl mx-auto lg:basis-[70%] md:basis-4/5 basis-[90%] pb-5 pt-3">
                         <div className="flex gap-x-2 mt-5">
                             {
-                                menuList.map((e,i)=>{
-                                    return(
+                                menuList.map((e, i) => {
+                                    return (
                                         <button key={i} className={`border py-1 px-2 rounded-md ${selectedCategory === i ? 'bg-cyan-600 text-white' : ''}`} onClick={() => setSelectedCategory(i)}>{e}</button>
                                     )
                                 })
                             }
                         </div>
-                    }                   
-                    <div>
-                        {
-                            dataResult && dataResult[selectedCategory].content.map((e,i)=>{
-                                return (
-                                    <div key={i} className="pt-3">
-                                        <p className='p-2 bg-cyan-600 text-white'>{e.name}</p>
-                                        <p className='pt-3'>{e.desc}</p>
-                                    </div>
-                                )
-                            })
-                        }              
+                        <div>
+                            {
+                                dataResult && dataResult[selectedCategory].content.map((e, i) => {
+                                    return (
+                                        <div key={i} className="pt-3">
+                                            <p className='p-2 bg-cyan-600 text-white'>{e.name}</p>
+                                            <p className='p-3 border'>{e.desc}</p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
-                </div>
+                }
             </div>
         </>
     )
